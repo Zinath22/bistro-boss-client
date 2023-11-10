ba27b6025f01e9af4f527eeaad567b714eef1f5e
Zinath22 <Zinath22@users.noreply.github.com>
2023-11-10 22:03:31 +0600

    first commit

---
+++ src/Router/Routes.jsx
@@ -0,0 +1,21 @@
+import {
+    createBrowserRouter,
+    
+  } from "react-router-dom";
+import MainLayout from "../MainLayout";
+import Home from "../Pages/Home/Home";
+
+  const Routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        }
+      ]
+    },
+  ]);
+
+  export default Routes;
