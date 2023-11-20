41b9006218253424809d5bab25dccbd334d0b5e7
Zinath22 <Zinath22@users.noreply.github.com>
2023-11-20 19:51:26 +0600

    payment system work start

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -18,6 +18,7 @@ import AddItems from "../Pages/Dashboard/AddItems/AddItems";
 import AdminRoute from "./AdminRoute";
 import ManageItems from "../Pages/Dashboard/ManageItems/ManageItems";
 import UpdateItem from "../Pages/Dashboard/UpdateItem/UpdateItem";
+import Payment from "../Pages/Dashboard/Payment/Payment";
 
   const Routes = createBrowserRouter([
     {
@@ -58,6 +59,10 @@ import UpdateItem from "../Pages/Dashboard/UpdateItem/UpdateItem";
         {
           path: 'cart',
           element: <Cart></Cart>
+        },
+        {
+          path: 'payment',
+          element: <Payment></Payment>
         },
 
         // admin routes 
